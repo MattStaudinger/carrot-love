ff31f35f002248f104acc47b5e322d0e246a2270
watnfoehn <hoehnefelix@googlemail.com>
2018-12-02 16:01:47 +0100

    fixing layout with grommet.io

--- client/src/components/pages/Collection.js
+++ client/src/components/pages/Collection.js
@@ -15,22 +15,13 @@ class Collection extends Component {
   render() {
     return (
       <div className="Collection">
-       {/*  <h2>Your collection of plants:</h2>
-        {this.state.plants.map(p =>
-        <div key={p._id}>
-          <Link to={`/plant/${p._id}`}><img src={p.picture_url} /></Link>
-          <Link to={`/plant/${p._id}`}><h3>{p.name}</h3></Link>
-          <p>next water appointment: WIP</p>
-        </div>)}
-        <Switch>
-          <Route path="/plant/:id" component={PlantDetail} />
-        </Switch> */}
-      <Box responsive='true'>
+      <Box align='center' margin='xsmall' pad='xsmall'>
       <Heading level={2}>Your collection of plants:</Heading>
+      </Box>
+      <Box direction='row-responsive' wrap='true' flex='shrink'>
       {this.state.plants.map(p =>
-        <Box key={p._id} 
-        border={{ color: '#78bc61', "size": "medium" }} basis='medium' margin='xsmall' >
-            <Image alignSelf='center' fit='contain' margin='xsmall' src={p.picture_url}>
+        <Box key={p._id} basis='medium' margin='xsmall' height='medium' border={{side: "top", color: '#78bc61', size: 'medium'}}>
+            <Image fit='contain' src={p.picture_url} margin='xsmall'>
               {/* <Link to={`/plant/${p._id}`}>1</Link> */}
               </Image>
           <Link to={`/plant/${p._id}`}><h3>{p.name}</h3></Link>
@@ -40,9 +31,6 @@ class Collection extends Component {
           <Route path="/plant/:id" component={PlantDetail} />
         </Switch>
       </Box>
-      {/* <Box gridArea='plant' background='brand' />
-      <Box gridArea='side' background='brand' />
-      <Box gridArea='foot' background='accent-1' /> */}
       </div>
     );
   }
